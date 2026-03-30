0a4a936fe2263e501bb4a31cad9bbf39281649d9
Emilio Sala <emilio_k88@hotmail.com>
2026-03-30 13:46:33 +0200

    ADD: middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express from "express";
+import { Request, Response } from "express";
+import { middlewareLogResponses } from "./middleware.js";
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponses);
+
+app.get("/healthz", handlerReadiness);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+function handlerReadiness(req: Request, res: Response) {
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.send("OK");
+}
