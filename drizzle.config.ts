d9c180cf932547180d5cc9cf86d762e0e4834c52
Emilio Sala <emilio_k88@hotmail.com>
2026-04-02 10:56:26 +0200

    ADD: config dbURL

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from "drizzle-kit";
-
-const DB_URL = process.env.DB_URL;
-
-if (!DB_URL) {
-  throw new Error("DB_URL is not defined");
-}
+import { config } from "./src/config";
 
 export default defineConfig({
   schema: "src/db/schema.ts",
   out: "src/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: DB_URL,
+    url: config.dbURL,
   },
 });
--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,17 @@
 type APIConfig = {
   fileserverHits: number;
+  dbURL: string;
 };
 
 export const config: APIConfig = {
   fileserverHits: 0,
+  dbURL: envOrThrow("DB_URL"),
 };
+
+function envOrThrow(key: string): string {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Missing environment variable: ${key}`);
+  }
+  return value;
+}
