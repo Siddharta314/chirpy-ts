6fa1738b0fd79cc9fa101a853991be08d11966d0
Emilio Sala <emilio_k88@hotmail.com>
2026-04-03 08:12:41 +0200

    ADD: hashed_password column to users

--- src/api/users.ts
+++ src/api/users.ts
@@ -2,17 +2,19 @@ import { Request, Response, NextFunction } from "express";
 import { Router } from "express";
 import { createUser } from "../db/queries/users.js";
 import { BadRequestError } from "../customError.js";
+import { hashPassword } from "../auth/index.js";
 export const userRouter = Router();
 
 userRouter.post(
   "/",
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { email } = req.body;
-      if (!email) {
+      const { email, password } = req.body;
+      if (!email || !password) {
         throw new BadRequestError("Missing required fields");
       }
-      const newUser = await createUser({ email });
+      const hashedPassword = await hashPassword(password);
+      const newUser = await createUser({ email, password: hashedPassword });
       res.status(201).json({
         id: newUser.id,
         email: newUser.email,
