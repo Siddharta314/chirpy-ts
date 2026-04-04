5a61a61709d00a7a9208a629ef997b49be3d1bdd
Emilio Sala <emilio_k88@hotmail.com>
2026-04-04 11:13:08 +0200

    FIX: bugs in validate JWT func

--- src/auth/jwt.ts
+++ src/auth/jwt.ts
@@ -1,5 +1,9 @@
-import { sign, verify } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+
+const { sign, verify } = jwt;
 import type { JwtPayload } from "jsonwebtoken";
+import { Request } from "express";
+import { UnauthorizedError } from "../customError.js";
 
 type payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
 
@@ -14,7 +18,6 @@ export function makeJWT(
       sub: userID,
       iss: "chirpy",
       iat,
-      exp: iat + expiresIn,
     } satisfies payload,
     secret,
     {
@@ -23,14 +26,51 @@ export function makeJWT(
   );
 }
 
+/*
+async version!
+export async function makeJWT(
+  userID: string,
+  expiresIn: number,
+  secret: string,
+): Promise<string> {
+  return new Promise((resolve, reject) => {
+    jwt.sign(
+      { sub: userID, iss: "chirpy" },
+      secret,
+      { expiresIn },
+      (err, token) => {
+        if (err || !token) return reject(err);
+        resolve(token);
+      }
+    );
+  });
+}
+*/
+
 export function validateJWT(tokenString: string, secret: string): string {
-  return verify(tokenString, secret) as string;
+  let payload: JwtPayload;
+  try {
+    payload = jwt.verify(tokenString, secret) as JwtPayload;
+  } catch {
+    throw new UnauthorizedError("Invalid token");
+  }
+
+  if (payload.iss !== "chirpy") {
+    throw new UnauthorizedError("Invalid issuer");
+  }
+  if (!payload.sub) {
+    throw new UnauthorizedError("No user ID in token");
+  }
+  return payload.sub;
 }
 
 export function getBearerToken(req: Request): string {
-  const authHeader = req.headers.get("Authorization");
+  const authHeader = req.headers["authorization"];
   if (!authHeader) {
     throw new Error("Authorization header is missing");
+  }
+  if (!authHeader.startsWith("Bearer ")) {
+    throw new Error("Authorization header is not a Bearer token");
   }
   const token = authHeader.split(" ")[1];
   if (!token) {
