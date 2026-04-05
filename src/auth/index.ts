53d1cb136f925d9cad460c6a2d1bfbff0f094bd4
Emilio Sala <emilio_k88@hotmail.com>
2026-04-05 10:37:21 +0200

    ADD: getAPIKey func

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,4 +1,5 @@
 import { hash, verify } from "argon2";
+import { UnauthorizedError } from "src/customError";
 
 export async function hashPassword(password: string): Promise<string> {
   return hash(password);
@@ -9,4 +10,19 @@ export async function checkPasswordHash(
   hash: string,
 ): Promise<boolean> {
   return verify(hash, password);
+}
+
+export function getAPIKey(req: Request): string {
+  const authHeader = req.headers.get("authorization");
+  if (!authHeader) {
+    throw new UnauthorizedError("Missing polka authorization");
+  }
+  if (!authHeader.startsWith("ApiKey ")) {
+    throw new UnauthorizedError("Invalid polka authorization");
+  }
+  const apiKey = authHeader.split(" ")[1];
+  if (!apiKey) {
+    throw new UnauthorizedError("Missing API key");
+  }
+  return apiKey;
 }
